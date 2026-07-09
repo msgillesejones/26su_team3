6bf610704c9cb3f0ef67ff3a2aa9d7a53c76d2a5
Gillese Jones <msgillesejones@gmail.com>
2026-07-09 10:58:29 -0400

    Initial commit

---
+++ src/main/java/com/example/_6su_team3/HelloController.java
@@ -0,0 +1,14 @@
+package com.example._6su_team3;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example._6su_team3 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example._6su_team3 to javafx.fxml;
+    exports com.example._6su_team3;
+}
